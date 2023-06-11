c7f91cc4b7154643665a110c797d11558325c723
phatgg221 <s3929218@rmit.edu.vn>
2023-06-11 20:01:53 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame windown= new JFrame();
+        windown.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windown.setResizable(false);
+        windown.setTitle("2D adventure");
+
+        windown.setLocationRelativeTo(null);
+        windown.setVisible(true);
+    }
+}
